568269bdd98e1279f08111e430a63da221eaf77e
Batuhan Dikmen <rbdikmen@gmail.com>
2023-12-09 06:21:49 +0300

    deployment

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use("/users", userRoute);
 //----------Deployment----------//
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/build")));
+  app.use(express.static(path.join(__dirname1, "/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname1, "dist", "index.html"));
   });
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'production',
@@ -9,4 +11,35 @@ module.exports = {
     filename: 'final.js',
   },
   target: 'node',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+    new webpack.IgnorePlugin({
+      checkResource(resource) {
+        const lazyImports = [
+          'bson-ext',
+          'kerberos',
+          '@mongodb-js/zstd',
+          'snappy',
+          'aws4',
+          'mongodb-client-encryption',
+          'bufferutil',
+          'utf-8-validate',
+          'snappy',
+          'snappy/package.json',
+        ];
+        if (!lazyImports.includes(resource)) {
+          return false;
+        }
+        try {
+          require.resolve(resource);
+        } catch (err) {
+          return true;
+        }
+        return false;
+      },
+    }),
+  ],
 };
